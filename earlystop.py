950c4e50987cb8734da8e2d2443de19acdcf7e36
januar-png <67256290+januar-png@users.noreply.github.com>
2023-11-30 11:03:23 +0800

    Update earlystop.py

--- earlystop.py
+++ earlystop.py
@@ -12,25 +12,25 @@ class Earlystop:
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         self.best_score = -np.inf
-        self.best_cost = np.inf
+        self.best_loss = np.inf
         self.counter = 0
-        self.train_costs = []
-        self.test_costs = []
+        self.train_losses = []  
+        self.val_losses = []    
         self.train_scores = []
-        self.test_scores = []
+        self.val_scores = []    
         self.epoch = 0
         self.best_epoch = 0
-        self.fig_cost = None
+        self.fig_loss = None    
         self.fig_score = None
         self.target_score = target_score
-        
-    def log(self, train_cost, test_cost, train_score, test_score):
-        self.train_costs.append(train_cost)
-        self.test_costs.append(test_cost)
+
+    def log(self, train_loss, val_loss, train_score, val_score):
+        self.train_losses.append(train_loss)  
+        self.val_losses.append(val_loss)      
         self.train_scores.append(train_score)
-        self.test_scores.append(test_score)
-        print(f"Epoch {len(self.train_costs)}: train_loss={train_cost:.4f}, val_loss={test_cost:.4f}, train_score={train_score:.4f}, val_score={test_score:.4f}")
-    
+        self.val_scores.append(val_score)
+        print(f"Epoch {len(self.train_losses)}: train_loss={train_loss:.4f}, val_loss={val_loss:.4f}, train_score={train_score:.4f}, val_score={val_score:.4f}")
+
     def save_checkpoint(self):
         state = {
             "model": self.model.state_dict(),
@@ -39,84 +39,83 @@ class Earlystop:
         }
         torch.save(state, os.path.join(self.outdir, "checkpoint.pth"))
         torch.save(self.config, os.path.join(self.outdir, "config.pth"))
-    
-    def early_stopping(self, model, monitor="test_score"):
+
+    def early_stopping(self, model, monitor="val_score"):  #
         stop = False
-        if monitor == "test_score":
-            reference = self.test_scores[-1] 
-            improve = reference > self.best_score 
-        elif monitor == "test_cost":
-            reference = self.test_costs[-1] 
-            improve = reference < self.best_cost
+        if monitor == "val_score":  
+            reference = self.val_scores[-1] 
+            improve = reference > self.best_score
+        elif monitor == "val_loss":  
+            reference = self.val_losses[-1]  
+            improve = reference < self.best_loss
         else:
-            raise Exception('Only supports monitor={"test_cost", "test_score"}')
-        
+            raise Exception('Only supports monitor={"val_loss", "val_score"}')
+
         if improve:
-            if monitor.endswith("_cost"):
-                self.best_cost = reference
+            if monitor.endswith("_loss"):
+                self.best_loss = reference
             elif monitor.endswith("_score"):
                 self.best_score = reference
 
             self.counter = 0
-            
+
             torch.save(model.state_dict(), os.path.join(self.outdir, "best_model.pth"))
         else:
             self.counter += 1
-            best = self.best_cost if monitor.endswith("_cost") else self.best_score
+            best = self.best_loss if monitor.endswith("_loss") else self.best_score
             self.best_epoch += 1
             print(f"EarlyStop patience = {self.counter:2}. Best {monitor}: {best:.4f}")
 
         if self.counter >= self.early_stop_patience or (self.target_score is not None and self.target_score <= reference):
-            print(f"Training early stopped. tidak ada peningkatan pada {self.early_stop_patience} epoch terakhir atau mencapai target score.")
-            print(f"Early Stopping pada epoch: {self.epoch} | Best {monitor}: {best:.4f}")
+            print(f"Training early stopped. No improvement in the last {self.early_stop_patience} epochs or reached target score.")
+            print(f"Early Stopping at epoch: {self.epoch} | Best {monitor}: {best:.4f}")
             stop = True
 
         self.epoch += 1
         return stop
-    
-    
+
     def reset_early_stop(self):
         self.counter = 0
-    
-    def plot_cost(self):
-        if self.fig_cost is None:
-            self.fig_cost = plt.figure()
-        plt.plot(self.train_costs, label="train")
-        plt.plot(self.test_costs, label="test")
+
+    def plot_loss(self):  
+        if self.fig_loss is None:
+            self.fig_loss = plt.figure()
+        plt.plot(self.train_losses, label="train")  
+        plt.plot(self.val_losses, label="val")     
         plt.legend()
-        plt.title("Cost vs. Epoch")
+        plt.title("Loss vs. Epoch")  
         plt.xlabel("Epoch")
-        plt.ylabel("Cost")
-        plt.savefig(os.path.join(self.outdir, "cost.png"))
+        plt.ylabel("Loss")  
+        plt.savefig(os.path.join(self.outdir, "loss.png"))
         plt.show()
-    
+
     def plot_score(self):
         if self.fig_score is None:
             self.fig_score = plt.figure()
         plt.plot(self.train_scores, label="train")
-        plt.plot(self.test_scores, label="test")
+        plt.plot(self.val_scores, label="val")
         plt.legend()
         plt.title("Score vs. Epoch")
         plt.xlabel("Epoch")
         plt.ylabel("Score")
         plt.savefig(os.path.join(self.outdir, "score.png"))
         plt.show()
-    
-    def cost_runtime_plotting(self):
+
+    def loss_runtime_plotting(self):  
         plt.ion()
-        plt.plot(self.train_costs, label="train")
-        plt.plot(self.test_costs, label="test")
+        plt.plot(self.train_losses, label="train")  
+        plt.plot(self.val_losses, label="val")      
         plt.legend()
-        plt.title("Cost vs. Epoch")
+        plt.title("Loss vs. Epoch") 
         plt.xlabel("Epoch")
-        plt.ylabel("Cost")
+        plt.ylabel("Loss")  
         plt.draw()
         plt.pause(0.001)
-    
+
     def score_runtime_plotting(self):
         plt.ion()
         plt.plot(self.train_scores, label="train")
-        plt.plot(self.test_scores, label="test")
+        plt.plot(self.val_scores, label="val")
         plt.legend()
         plt.title("Score vs. Epoch")
         plt.xlabel("Epoch")
